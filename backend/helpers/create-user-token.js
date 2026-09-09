7aef4976597522d856bc6d3ef4c7ddde90795c90
Fernando <fernandomorenosilva10@gmail.com>
2026-09-09 16:33:30 -0300

    Revisando autenticacao de usuarios

--- backend/helpers/create-user-token.js
+++ backend/helpers/create-user-token.js
@@ -1,24 +1,28 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const createUserToken = async (user, req, res) => {
-  const token = jwt.sign(
-    // payload data
-    {
-      id_number: user.id,
-      name: user.nome,
-      id: user.email,
-      tipo: user.tipo_usuario
 
-    },
-    process.env.JWT_SECRET
-  );
+    const token = jwt.sign(
+        {
+            id_number: user.id,
+            name: user.nome,
+            id: user.email,
+            tipo: user.tipo_usuario
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "8h"
+        }
+    );
 
-  // return token
-  res.status(200).json({
-    message: "Você está autenticado!",
-    token: token,
-    userId: user.email,
-  });
+
+    return res.status(200).json({
+        message: "Login realizado com sucesso!",
+        token,
+        userId: user.id,
+        tipoUsuario: user.tipo_usuario
+    });
+
 };
 
 export default createUserToken;
